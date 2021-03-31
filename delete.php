dac3e06e11ba5a61c71c92da7ce7f3166776f3c8
gnorgol <gnorgol@outlook.com>
2021-03-31 09:21:54 +0200

    correction ;

--- delete.php
+++ delete.php
@@ -2,4 +2,4 @@
 require_once("Fonctions/fonction.php");
 $connexion = connexionMysqlBdd("localhost","double", "root", "");
 delete($connexion,$_GET['id']);
-header('Location: listeCours.php')
+header('Location: listeCours.php');
--- verif.php
+++ verif.php
@@ -3,5 +3,5 @@ require_once("Fonctions/fonction.php");
 $connexion = connexionMysqlBdd("localhost","double", "root", "");
 AddCouple($_POST['intitule'],$_POST['niveau'],$_POST['texte'],$connexion);
 
-header('Location: ajoutCours.php')
+header('Location: ajoutCours.php');
 
