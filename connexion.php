df2cc9239a57ec5b70cdb163ed7edb0ac4da9a99
tessier-benjamin <be.tessier@free.fr>
2021-04-05 16:31:02 +0200

    css

--- connexion.php
+++ connexion.php
@@ -1,14 +1,16 @@
 <html>
-<link href="css/cssAjoutCours.css" rel="stylesheet">
+<link href="css/cssConnexion.css" rel="stylesheet">
 <form method="post" action="verifCo.php">
-    <div>
+    <div class="nom">
         <label >Nom:</label>
         <input type="text" id="name" name="name">
     </div>
-    <div>
+    <div class="mdp">
         <label >Mot de passe:</label>
         <input type="password" id="mdp" name="mdp">
     </div>
-    <input type="submit" value = "Connexion"/>
+    <div>
+    <input class="button" type="submit" value = "Connexion"/>
+    </div>
 </form>
 </html>
