fe035724dd4b0630cabb1aaff72243b5e56b694a
tessier-benjamin <be.tessier@free.fr>
2021-04-18 01:02:18 +0200

    css

--- EditUser.php
+++ EditUser.php
@@ -6,25 +6,26 @@ $connexion = connexionMysqlBdd("localhost","double", "root", "");
 $données = GetUsersId($_GET['id'],$connexion);
 $permi = GetPermission($connexion);
 ?>
-<link href="css/cssAjoutCours.css" rel="stylesheet">
+<link href="css/cssEditUser.css" rel="stylesheet">
 <form method="post" action="UpdateName.php">
-
+<div class="nom">
 <label >Nom :</label>
         <input type="hidden" name="id" value="<?php echo $_GET['id']?>" />
         <input type="text" id="name" name="name" value="<?php echo $données[0]['Name'] ?>">
         <input type="submit" value = "Modifier Nom"/>
-
+</div>
 </form>
 <form method="post" action="UpdateMDP.php">
-
+<div class="motdepasse">
 <label >Mot de passe :</label>
         <input type="hidden" name="id" value="<?php echo $_GET['id']?>" />
         <input type="password" id="mdp" name="mdp" value="">
         <input type="submit" value = "Modifier mot de passe"/>
 </form>
-
+</div>
 <form method="post" action="UpdatePerm.php">
 <input type="hidden" name="id" value="<?php echo $_GET['id']?>" />
+<div class="permission">
 <label>Permission:</label>
     <select name = "perm">
         <?php
@@ -40,8 +41,9 @@ $permi = GetPermission($connexion);
         ?>
     </select>
     <input type="submit" value = "Modifier les permission"/>
+    </div>
 </form>
 
 </br>
-<a href="index.php">retour</a>
+<a href="index.php"><img class="image" src="images/fleche.jpg" /></a>
 </html>
