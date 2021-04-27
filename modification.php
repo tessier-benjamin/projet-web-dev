3da2145fe5a9e29e401f56e157fb1792e6d09229
tessier-benjamin <52489423+tessier-benjamin@users.noreply.github.com>
2021-04-27 14:32:20 +0200

    css

--- modification.php
+++ modification.php
@@ -6,32 +6,32 @@ $connexion = connexionMysqlBdd("localhost","double", "root", "");
 $données = GetCours($connexion,$_GET['id']);
 ?>
 
-<link href="css/cssAjoutCours.css" rel="stylesheet">
+<link href="css/cssModification.css" rel="stylesheet">
 <script type="text/javascript" src="infobulle.js"></script>
 <form method="post" action="update.php">
-    <div>
+    <div class="intitule">
         <input type="hidden" name="id" value="<?php echo $_GET['id']?>" />
         <label >intitule:</label>
         <input type="text" id="intitule" name="intitule" value="<?php echo $données[0]['intitule'] ?>">
     </div>
-    <div>
+    <div class="niveau">
         <label >niveau:</label>
         <input type="text" id="niveau" name="niveau" value="<?php echo $données[0]['niveau'] ?>">
     </div>
-    <div>
+    <div class="liste">
         <label >liste des couples :</label>
         <textarea type="text" id="texte" name="texte"><?php echo $données[0]['couple'] ?></textarea>
         <img src="images/infobull.png" alt="infobulle" onmouseover="javascript: afficher_aide(document.getElementById('aide_salaire'));" onmouseout="javascript: afficher_aide(document.getElementById('aide_salaire'));"/>
         <div class="infobulle">
             <div class="infobulle-texte" id="aide_salaire" style="display: none;">
-                séparer chaque couple par un point virgule et chaque chiffre du couple par un "+"
+                separer chaque couple par un point virgule et chaque chiffre du couple par un "+"
             </div>
         </div>
     </div> 
-	<div class="button">
-		<input type="submit" value="Envoyer le message" /> 
+	<div>
+		<input class="button" type="submit" value="Envoyer le message" /> 
     </div>
 </form>
 </br>
-<a href="listeCours.php">retour</a>
+<a href="index.php"><img class="image" src="images/fleche.jpg" /></a>
 </html>
